37f3c1386e6e571b56604299bff15063d878b0d0
Tahira Nawaz <tahiranawaz@teresol.com>
2023-06-15 15:58:21 +0500

    added new projects

--- src/views/LiveProjectView.vue
+++ src/views/LiveProjectView.vue
@@ -1,25 +1,36 @@
 <template>
- 
-    <p class="text-center mb-3 dark:text-purple-300">Demo for Projects </p>
-    <div class="max-h-[50vh] overflow-auto sm:w-4/12 mx-auto">
-    
-            <!-- <a :href="project.html_url" target="_bl"> -->
-                <a class="font-bold text-xl"  href="https://simplevuetodolist.netlify.app">
-                <div class="bg-purple-300 mb-4 p-2 rounded-md shadow-md">
-                    Todo List
-                </div>
-               </a>
-
-                <div class="bg-purple-300 mb-4 p-2 rounded-md shadow-md">
-                    <a class="font-bold text-xl" href="https://simplevuetodolist.netlify.app"> List</a>
-                 
-                </div>
-                
-            <!-- </a> -->
-      
+  <p class="text-center mb-3 dark:text-purple-300">Demo for Projects</p>
+  <div class="max-h-[50vh] overflow-auto sm:w-4/12 mx-auto">
+    <div class="bg-purple-300 mb-4 p-2 rounded-md shadow-md">
+      <a
+        class="font-bold text-xl"
+        href="https://vuejs-e-com-website.netlify.app/"
+      >
+        E-commerce Website
+      </a>
     </div>
+  
+<div class="bg-purple-300 mb-4 p-2 rounded-md shadow-md">
+      <a
+        class="font-bold text-xl"
+        href="https://tahiranawazportfolio.netlify.app"
+      >
+        Portfolio</a
+      >
+    </div>
+    <div class="bg-purple-300 mb-4 p-2 rounded-md shadow-md">
+    <a class="font-bold text-xl" href="https://simplevuetodolist.netlify.app">
+     Todo List
+    </a>
+</div>
+    <div class="bg-purple-300 mb-4 p-2 rounded-md shadow-md">
+      <a
+        class="font-bold text-xl"
+        href="https://countdown-vuejs-app.netlify.app"
+      >
+        CountDown App</a
+      >
+    </div>
+  </div>
 </template>
-<script setup>
-
-
-</script>
+<script setup></script>
